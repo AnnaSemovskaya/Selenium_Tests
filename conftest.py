b4d085dfb078550a14f9c464fbe18598cf1f6ed7
AnnaSemovskaya <68237642+AnnaSemovskaya@users.noreply.github.com>
2021-11-30 16:09:00 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en-gb',
+                     help="Choose language in standart format, en-gb is by default")
+
+
+@pytest.fixture(scope="function")
+def browser(request):   
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.fixture(scope="function")
+def language(request):
+    language = request.config.getoption("language")   
+    print(language)
+    return(language)
---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+import pytest
+
+def test_find_basket_button(browser, language):
+    print(str(language) + " inside")
+    longlink=f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    browser.implicitly_wait(10)
+    browser.get(longlink)
+    buttons=browser.find_elements_by_css_selector(".btn-add-to-basket")
+    assert len(buttons) != 0, "No button 'ADD TO BASKET' on the page " + longlink
+    time.sleep(15)
